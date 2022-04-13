153e4d94d9128911cceb8ffb6727fbd6e86ab9e7
Lucas Alves <lc_2014@hotmail.com>
2022-04-13 15:12:32 -0300

    add delete function

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,10 +4,14 @@ import Box from "./components/Box"
 import NavBar from "./components/NavBar"
 import { db } from "./firebase"
 import { uid } from "uid"
-import { onValue, ref, set } from "firebase/database"
+import { onValue, ref, remove, set, update } from "firebase/database"
 import { useEffect } from "react"
+import { FaRegTrashAlt } from "react-icons/fa"
+import { FaEdit } from "react-icons/fa"
 
 export default function Home(){
+
+  const date = new Date().toString();
 
   const [input, setInput] = useState("")
   const [posts, setPost] = useState([])
@@ -16,6 +20,8 @@ export default function Home(){
     getPost()
   },[])
 
+
+  // pega os dados do firebase
   function getPost(){
     onValue(ref(db),(snapshot)=>{
       setPost([])
@@ -28,26 +34,52 @@ export default function Home(){
       
     })
   }
+  // fim
  
+  // pega o que foi digitado
   const handleChange = (e) =>{
     setInput(e.target.value)
   }
+  // fim
 
+  // update
+
+  const handlerUpdate = (post) =>{
+    update(ref(db, `/${post.date}`),{
+      "input": "updated",
+    })
+  }
+
+  // fim
+
+  // deleta dados
+
+  const handlerDelete = (post) =>{
+    remove(ref(db, `/${post.date}` ))
+  }
+
+  // fim
+
+  // adiciona dados no firebase
   const writeDataBase = () =>{
     const id = uid()
-    const dt = new Date();
+    
    
   
     if(input !== ""){
-      set(ref(db, `/${dt}`),{
+      set(ref(db, `/${date}`),{
         input,
+        date,
         id
       })
       setInput("")
+      
     }else{
       alert("Digite algo")
     }
+    
   }
+  // fim
   return (
     <div>
       <NavBar
@@ -55,21 +87,37 @@ export default function Home(){
       />
       <div className={style.container}>
         <div>
+
           <Box>
             <div className={style.inputBox}>
       
-                <textarea onChange={handleChange} name="" id="" cols="30" rows="3"></textarea>
+                <textarea onChange={handleChange} value={input} name="" id="" cols="30" rows="3"></textarea>
                 <button onClick={writeDataBase}>Publicar</button>
  
             </div>
           </Box>
+
           <div>
               {posts.map((post, id) =>
                 <Box key={id}>
-                  <div><h2>{post.input}</h2></div>
+
+                  <div className={style.post}>
+
+                    <h2>{post.input}</h2>
+                    <div>
+                      <button onClick={() => handlerDelete(post)}><FaRegTrashAlt/></button>
+                      <button onClick={() => handlerUpdate(post)}><FaEdit/></button>
+                    </div>
+                    
+
+                  </div>
+
+                  <span>{post.date}</span>
+
                 </Box>
               )}
           </div>
+
         </div>
       </div>
     </div>
