f3469fbcea2d360dd7dd731da9afd079d6de3b79
Lucas Alves <46626924+LukazAlvez@users.noreply.github.com>
2022-06-08 19:00:36 -0300

    Update NavBar.jsx

--- pages/components/NavBar.jsx
+++ pages/components/NavBar.jsx
@@ -5,13 +5,7 @@ export default function NavBar(props){
         <div style={{borderBottom: "1px solid var(--cor-font)"}}>
         <div className={style.navBar}>
           <h2>{props.logo ?? "Logo"}</h2>
-          <div>
-            <ol>
-              <li ><a href="#">Home</a></li>
-              <li ><a href="#">Sobre</a></li>
-              <li ><a href="#">Sair</a></li>
-            </ol>
-          </div>
+        
         </div>
       </div>
     )
